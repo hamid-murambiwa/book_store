5bd0de696df2c23131a9ca9a00845c1976917a51
hamid-murambiwa <machipisajunior@gmail.com>
2021-12-07 08:05:52 +0200

    Made the following changes: Add components folder Add form.js and navbar.js files to components folder Add Books and Categories pages to the app using React router Add README.md file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import NavBar from './components/navbar';
+import AddBook from './components/form'
+
+function App() {
+  return (
+    <section>
+     <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Books />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
+      <AddBook />
+    </section>
+  );
+}
+
+export default App;
